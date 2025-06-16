3ac8e272c3220af2101ee5db1bbe297d01617726
Orifjon Kenjayev <128310421+OrifjonKenjayev@users.noreply.github.com>
2025-06-16 21:11:35 +0500

    Add files via upload

---
+++ code/R2.R
@@ -0,0 +1,40 @@
+library(dslabs)
+library(tidyverse)
+a <- 1
+print(a)
+a
+b <- 2
+c <- -5
+c
+
+
+ls()
+
+sqrt(b)
+
+gamma <- (-5 + sqrt(b) + 3^2)
+print(gamma)
+log(2.7)
+
+?log
+log10(1e7) # = 7
+args(log)
+
+
+
+
+
+
+
+
+
+
+log(81, base = 3)
+log(base = 3 , 243)
+
+log(8,2)
+
+help("+")
+
+
+data()

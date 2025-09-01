a07e44adc1b9d221114f313c3ab8c0a4bc3aafd0
Orifjon Kenjayev <128310421+OrifjonKenjayev@users.noreply.github.com>
2025-09-01 18:35:07 +0500

    Add files via upload

---
+++ code/R4.R
@@ -0,0 +1,85 @@
+# Vector 3.7
+c
+codes <- c(380,124,818)
+codes
+class(codes)
+country <- c("italy", "canada", "egypt")
+country
+class(country)
+country <- c('italy', 'canada', 'egypt')
+country
+
+
+
+codes = c(italy = 1, canada = 2, egypt = 3)
+codes
+
+class(codes)
+
+names(codes)
+
+
+codes = c("italy" = 1, "canada" = 2, "egypt" = 3)
+
+
+
+codes
+
+
+
+raqam <- c(4,5,6)
+davlat <- c("angliya", "misr", "italiya")
+
+names(raqam) <- davlat
+raqam
+
+
+
+
+
+seq(1,10)
+
+seq(1,10,2)
+
+10:20
+class(1:10)
+
+
+class(seq(1,20, 0.5))
+
+
+
+
+
+
+
+raqam
+
+
+
+raqam[2]
+raqam[c(1,3)]
+
+
+
+raqam[1:2]
+raqam["angliya"]
+raqam[c("angliya", "misr")]
+
+
+
+x <- c(1,"angliya", 3)
+x
+class(x)
+
+
+x <- 1:5
+x
+y <- as.character(x)
+y
+
+as.numeric(y)
+
+
+x <- c("1", "b", "3")
+as.numeric(x)
